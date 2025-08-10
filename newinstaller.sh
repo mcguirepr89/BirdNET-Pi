396f58861a13f21cce174291b3363417f66be34b
Patrick McGuire <60325264+mcguirepr89@users.noreply.github.com>
2025-08-10 07:36:42 -0700

    Update newinstaller.sh

--- newinstaller.sh
+++ newinstaller.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-echo "
-
-Hello -- this fork has been archived. Please issue the new installation command:
+echo "\n\nHello -- this fork has been archived. Please issue the new installation command:
 
 curl -s https://raw.githubusercontent.com/Nachtzuster/BirdNET-Pi/main/newinstaller.sh | bash
 
